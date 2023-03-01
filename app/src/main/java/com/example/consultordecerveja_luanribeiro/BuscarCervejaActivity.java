0c3f8b71be81d388004a1fe42d8583b744e07014
Luan de Castro Ribeiro <luan.ribeiro@estudante.ifgoiano.edu.br>
2023-03-01 10:37:24 -0300

    Adding spinner items

--- app/src/main/java/com/example/consultordecerveja_luanribeiro/BuscarCervejaActivity.java
+++ app/src/main/java/com/example/consultordecerveja_luanribeiro/BuscarCervejaActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class BuscarCervejaActivity extends AppCompatActivity {
@@ -13,11 +15,20 @@ public class BuscarCervejaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_cerveja);
+
         Button backButton = findViewById(R.id.backButton);
         String username = getIntent().getStringExtra("userName");
         String greetingsMsg = String.format("Olá, %s, seja bem vindo!", username);
         TextView greetingsText = findViewById(R.id.greetingsText);
+
         greetingsText.setText(greetingsMsg);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_cervejas, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         backButton.setOnClickListener(view -> {
             Intent intent = new Intent(BuscarCervejaActivity.this, MainActivity.class);
             startActivity(intent);
--- app/src/main/java/com/example/consultordecerveja_luanribeiro/MainActivity.java
+++ app/src/main/java/com/example/consultordecerveja_luanribeiro/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.consultordecerveja_luanribeiro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("userName", textField.getText().toString());
                 System.out.println(textField.getText().toString());
                 startActivity(intent);
-            }else
+            } else
                 errorMessage.setText("Digite um nome válido!");
         });
     }
